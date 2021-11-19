1a4bed8d3ef2c958685e1267edb083807cb0ab1a
Jason Anderson <jasonanderson@uchicago.edu>
2021-11-19 17:47:21 -0600

    containerd fixes

--- k3s/entrypoint.sh
+++ k3s/entrypoint.sh
@@ -3,6 +3,7 @@
 # Balena will mount a socket and set DOCKER_HOST to
 # point to the socket path.
 if [ -n "$DOCKER_HOST" ]; then
+  mkdir -p /run/k3s/containerd
   ln -sf "${DOCKER_HOST##unix://}" /run/k3s/containerd/containerd.sock
 fi
 
