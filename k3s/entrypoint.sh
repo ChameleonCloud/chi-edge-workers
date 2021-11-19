2c35954da3389c4cccd76bd9378c97c6b075404e
Jason Anderson <jasonanderson@uchicago.edu>
2021-11-19 16:22:05 -0600

    do not do symlink

--- k3s/entrypoint.sh
+++ k3s/entrypoint.sh
@@ -3,7 +3,7 @@
 # Balena will mount a socket and set DOCKER_HOST to
 # point to the socket path.
 if [ -n "$DOCKER_HOST" ]; then
-  ln -sf "${DOCKER_HOST##unix://}" /var/run/docker.sock 
+  #ln -sf "${DOCKER_HOST##unix://}" /var/run/docker.sock 
 fi
 
 exec k3s "$@"
