eab8c713a4d6a9dd8237d52e0edf34caf47d9614
Jason Anderson <jasonanderson@uchicago.edu>
2021-11-22 17:08:51 -0600

    fixup

--- k3s/entrypoint.sh
+++ k3s/entrypoint.sh
@@ -6,11 +6,9 @@ set -o nounset
 
 # Balena will mount a socket and set DOCKER_HOST to
 # point to the socket path.
-if [ -n "$DOCKER_HOST" ]; then
-  mkdir -p /run/k3s/containerd
-  ln -sf "${DOCKER_HOST##unix://}" /run/k3s/containerd/containerd.sock
-  ln -sf "${DOCKER_HOST##unix://}" /var/run/docker.sock
-fi
+#mkdir -p /run/k3s/containerd
+#ln -sf "${DOCKER_HOST##unix://}" /run/k3s/containerd/containerd.sock
+ln -sf "${DOCKER_HOST##unix://}" /var/run/docker.sock
 
 ##############
 # DISCLAIMER
