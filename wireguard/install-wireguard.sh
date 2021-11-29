d7a36a7083ff7cf5624a672337971b818096095f
Jason Anderson <jasonanderson@uchicago.edu>
2021-11-29 16:24:09 -0600

    don't rely on detecting kernel support at build time
    We could be building anywhere, doesn't mean the device will have
    Wireguard on it!

--- wireguard/install-wireguard.sh
+++ wireguard/install-wireguard.sh
@@ -1,43 +1,48 @@
 #!/bin/sh
 set -o errexit
 
-env | grep BALENA_
-
 no_kmod() {
+  echo "The target host device _should_ have kernel support. Skipping kernel module build."
+  echo "Writing flag file"
   echo 0>$(pwd)/kmod_built
   mkdir -p $(pwd)/tools
   exit 0
 }
 
-if modprobe wireguard 2>/dev/null; then
-  echo "Native Wireguard support detected, not building kernel module."
-  no_kmod
-elif [ "$BALENA_MACHINE_NAME" = "raspberrypi4-64" ]; then
-  echo "Raspberry Pi 4 (64-bit OS) detected. This host device _should_ "
-  echo "have kernel support, but it was not detected in the build host. "
-  echo "Exiting and assuming that things will 'just work' on the device."
-  no_kmod
-else
-  echo "No native Wireguard support detected, building kernal module from source."
-fi
-
-VERSION="${BALENA_HOST_OS_VERSION#balenaOS }".dev
-
-git clone https://git.zx2c4.com/wireguard-linux-compat
-git clone https://git.zx2c4.com/wireguard-tools
+case "$BALENA_MACHINE_NAME" in
+  raspberrypi4-64)
+    no_kmod
+    ;;
+  raspberrypi3-64)
+    OS_VERSION=2.80.3+rev1
+    ;;
+  *)
+    echo "Unsupported device type '$BALENA_MACHINE_NAME'"
+    exit 1
+    ;;
+esac
 
 balena_images="https://files.balena-cloud.com/images"
-km_source="$balena_images/$BALENA_MACHINE_NAME/$VERSION/kernel_modules_headers.tar.gz"
+km_source="$balena_images/$BALENA_MACHINE_NAME/$OS_VERSION".dev/kernel_modules_headers.tar.gz
+
 echo "Getting kernel modules from $km_source"
 headers_tarball="$(echo "$km_source" | sed -e 's/+/%2B/')"
 curl -SsL -o headers.tar.gz "$headers_tarball"
 tar -xf headers.tar.gz
 
+echo "Getting Wireguard kernel source"
+git clone https://git.zx2c4.com/wireguard-linux-compat
+git clone https://git.zx2c4.com/wireguard-tools
+
+echo "Compiling kernel module"
 make -C kernel_modules_headers -j$(nproc) modules_prepare
 make -C kernel_modules_headers M=$(pwd)/wireguard-linux-compat/src -j$(nproc)
 cp wireguard-linux-compat/src/wireguard.ko .
 
+echo "Compiling tools"
 make -C $(pwd)/wireguard-tools/src -j$(nproc)
 mkdir -p $(pwd)/tools
 make -C $(pwd)/wireguard-tools/src DESTDIR=$(pwd)/tools install
+
+echo "Writing flag file"
 echo 1>$(pwd)/kmod_built
