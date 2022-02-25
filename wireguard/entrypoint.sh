ca4e392ae35ac495b7f992bdc40e6264ad849a6d
Jason Anderson <jasonanderson@uchicago.edu>
2022-02-25 17:08:31 -0600

    use lsmod instead of modprobe

--- wireguard/entrypoint.sh
+++ wireguard/entrypoint.sh
@@ -2,7 +2,7 @@
 
 modprobe udp_tunnel
 modprobe ip6_udp_tunnel
-modprobe wireguard || {
+lsmod | grep -q wireguard || {
   # If the kernel module wasn't available, we would have built it here
   insmod /wireguard/wireguard.ko || true
 }
