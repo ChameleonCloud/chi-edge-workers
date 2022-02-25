1a3e3f0d63c4922255004ebb9c4f35a484a2503f
Jason Anderson <jasonanderson@uchicago.edu>
2022-02-25 17:15:34 -0600

    handle nonzero exit on read

--- wireguard/start.sh
+++ wireguard/start.sh
@@ -11,7 +11,7 @@ wg_up() {
   ip link del dev "$iface" 2>/dev/null || true
   ip link add dev "$iface" type wireguard
   wg_conf=/etc/wireguard/"$iface".conf
-  read wg_ipv4 </etc/wireguard/"$iface".ipv4
+  read wg_ipv4 </etc/wireguard/"$iface".ipv4 || true
   wg syncconf "$iface" "$wg_conf"
   ip address add "$wg_ipv4" dev "$iface"
   ip link set up dev "$iface"
