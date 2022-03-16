3144d7a5c5b3db3c00ce0b7d57bf9e5be0accc57
Jason Anderson <jasonanderson@uchicago.edu>
2022-03-16 15:58:37 -0500

    restart wireguard if conf changes

--- coordinator/coordinator.py
+++ coordinator/coordinator.py
@@ -124,11 +124,52 @@ def sync_wireguard_config(channel, private_key_s):
         wg_restart = True
 
     if wg_restart:
-        # TODO: trigger restart of wireguard service
-        pass
+        restart_service("wireguard")
 
 
 def sync_device_name(hardware, balena_device_name):
+    hardware_name = hardware["name"]
+    if hardware_name == balena_device_name:
+        return
+
+    if os.getenv("BALENA_SUPERVISOR_OVERRIDE_LOCK") != "1":
+        print("Not updating hostname because update lock is in place")
+        return
+    print(f"Updating device hostname to {hardware_name}")
+    call_supervisor(
+        "/v1/device/host-config",
+        method="patch",
+        json={
+            "network": {
+                "hostname": hardware["name"],
+            },
+        },
+    )
+
+
+def restart_service(service_name):
+    status = call_supervisor("/v2/state/status")
+    running_service = next(
+        iter(
+            c["status"] == "Running"
+            for c in status["containers"]
+            if c["serviceName"] == service_name
+        ),
+        None,
+    )
+    # Only restart it if it was not explicitly stopped or is updating
+    if running_service:
+        print(f"Restarting {service_name} service")
+        call_supervisor(
+            f"/v2/applications/{running_service['appId']}/restart-service",
+            method="post",
+            json={
+                "serviceName": service_name,
+            },
+        )
+
+
+def call_supervisor(path, method="get", json=None):
     # If the Balena device name does not match, update it from hardware via
     # calling the Balena supervisor.
     supervisor_api_url = os.getenv("BALENA_SUPERVISOR_ADDRESS")
@@ -136,21 +177,11 @@ def sync_device_name(hardware, balena_device_name):
     if not (supervisor_api_url and supervisor_api_key):
         raise RuntimeError("Missing Balena supervisor configuration")
 
-    hardware_name = hardware["name"]
-    if hardware_name != balena_device_name:
-        if os.getenv("BALENA_SUPERVISOR_OVERRIDE_LOCK") != "1":
-            print("Not updating hostname because update lock is in place")
-            return
-        print(f"Updating device hostname to {hardware_name}")
-        res = requests.patch(
-            f"{supervisor_api_url}/v1/device/host-config",
-            json={
-                "network": {
-                    "hostname": hardware["name"],
-                },
-            },
-        )
-        res.raise_for_status()
+    res = requests.request(
+        method, f"{supervisor_api_url}{path}?apikey={supervisor_api_key}", json=json
+    )
+    res.raise_for_status()
+    return res.json()
 
 
 def get_doni_client():
