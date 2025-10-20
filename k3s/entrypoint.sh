fdc508a5b5dd5929dedf20762bb9c1e5589d1d87
Michael Sherman <shermanm@uchicago.edu>
2025-10-20 17:23:41 -0500

    minimal k3s

--- k3s/entrypoint.sh
+++ k3s/entrypoint.sh
@@ -2,39 +2,39 @@
 set -o errexit
 set -o nounset
 
-# If cgroups v2 are enabled, ensure nesting compatibility.
-# NOTE: this may not be necessary anymore in K3s due to
-# https://github.com/k3s-io/k3s/pull/4086
 #########################################################################################################################################
 # DISCLAIMER																																																														#
 # Copied from https://github.com/moby/moby/blob/ed89041433a031cafc0a0f19cfe573c31688d377/hack/dind#L28-L37															#
-# Permission granted by Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp> (https://github.com/rancher/k3d/issues/493#issuecomment-827405962)	#
+# Permission granted by Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp> (https://github.com/k3d-io/k3d/issues/493#issuecomment-827405962)	#
 # Moby License Apache 2.0: https://github.com/moby/moby/blob/ed89041433a031cafc0a0f19cfe573c31688d377/LICENSE														#
 #########################################################################################################################################
 if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
-	# move the processes from the root group to the /init group,
+  echo "[$(date -Iseconds)] [CgroupV2 Fix] Evacuating Root Cgroup ..."
+  # move the processes from the root group to the /init group,
   # otherwise writing subtree_control fails with EBUSY.
   mkdir -p /sys/fs/cgroup/init
-  busybox xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
+  # new k3s releases only have xargs from findutils
+  if command -v xargs >/dev/null; then
+    xargs -rn1 </sys/fs/cgroup/cgroup.procs >/sys/fs/cgroup/init/cgroup.procs || :
+  else
+    busybox xargs -rn1 </sys/fs/cgroup/cgroup.procs >/sys/fs/cgroup/init/cgroup.procs || :
+  fi
   # enable controllers
   sed -e 's/ / +/g' -e 's/^/+/' <"/sys/fs/cgroup/cgroup.controllers" >"/sys/fs/cgroup/cgroup.subtree_control"
+  echo "[$(date -Iseconds)] [CgroupV2 Fix] Done"
 fi
 
-if [ "${DOCKER_REGISTRY:-x}" != "x" ]; then
-  mkdir -p /etc/rancher/k3s
-  cat >/etc/rancher/k3s/registries.yaml <<EOF
-mirrors:
-  docker.io:
-    endpoint:
-      - "$DOCKER_REGISTRY"
-EOF
+echo "[$(date -Iseconds)] [Mounts] Fixing Mountpoints..."
+if command -v mount >/dev/null; then
+  mount --make-rshared / || :
+else
+  busybox mount --make-rshared / || :
 fi
 
 # looking up IP address from wireguard interface
 WG_ADDRESS="$(ip -brief address show wg-calico | awk '{print $3}' | cut -d '/' -f 1)"
 
-k3s agent \
-  --bind-address "${WG_ADDRESS}" \
-  --node-ip "${WG_ADDRESS}" \
-  --kubelet-arg=volume-plugin-dir=/opt/libexec/kubernetes/kubelet-plugins/volume/exec \
-  "$@"
+# use exec to run the other commands so that signals are passed correctly
+exec /bin/k3s agent \
+  --bind-address	"${WG_ADDRESS}" \
+  --node-ip "${WG_ADDRESS}"
